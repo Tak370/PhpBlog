ed4705b8cd26d7a7e85d8f8e8a5ff420c016aaec
Laurent Legrand <laurentlegrandpro@icloud.com>
2020-09-17 13:39:03 +0200

    modify basic structures files

--- src/controller/post/index.php
+++ src/controller/post/index.php
@@ -1,13 +1,9 @@
 <?php
 use App\Connection;
-use App\Table\PostTable;
 
 $title = 'Mon Blog';
 $pdo = Connection::getPDO();
 
-$table = new PostTable($pdo);
-[$posts, $pagination] = $table->findPaginated();
-
-$link = $router->url('home');
+//$link = $router->url('home');
 
 require '../view/post/index.php';
--- view/layout/default.php
+++ view/layout/default.php
@@ -18,9 +18,7 @@
 
     <footer class="bg-light py-4 footer mt-auto">
         <div class="container">
-            <?php if (defined('DEBUG_TIME')): ?>
-            Page générée en <?= round(1000 * (microtime(true) - DEBUG_TIME)) ?>ms
-            <?php endif ?>
+
         </div>
     </footer>
 </body>
