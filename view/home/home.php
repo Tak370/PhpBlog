a6161a5f9ba35072e1ac9da2e79e7e3a5fcd1f46
Laurent Legrand <laurentlegrandpro@icloud.com>
2020-10-07 17:18:38 +0200

    swiftmailer in controller

--- view/home/home.php
+++ view/home/home.php
@@ -101,29 +101,7 @@
     <div class="container">
         <div class="red-divider"></div>
 
-        <?php
-        if(isset($_POST['sendmail'])) {
-// Create the Transport
-            $transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
-                ->setUsername('llegrandcontact@gmail.com')
-                ->setPassword('brvuxgdvdngwgmst')
-            ;
 
-// Create the Mailer using your created Transport
-            $mailer = new Swift_Mailer($transport);
-
-// Create a message
-            $message = (new Swift_Message('Contact'))
-                ->setFrom(['llegrandcontact@gmail.com' => 'Laurent Legrand'])
-                ->setTo(['laurentlegrandcompte@icloud.com'])
-                ->setReplyTo($_POST['email'], $_POST['name'])
-                ->setBody($_POST['message'])
-            ;
-
-// Send the message
-            $result = $mailer->send($message);
-        }
-        ?>
 
         <div class="heading">
             <h2>Contactez-moi</h2>
