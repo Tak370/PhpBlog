84c09f822d8fe45a1d72922a250daa93059f3f64
Laurent Legrand <laurentlegrandpro@icloud.com>
2020-10-10 10:22:57 +0200

    swiftmailer configuration improved

--- service/swiftmailer.php
+++ service/swiftmailer.php
@@ -1,19 +1,21 @@
 <?php
 
+require '../config/swiftmailerConfig.php';
+
 // Send an Email using SwiftMailer
 if (isset($_POST['sendmail'])) {
 // Create the Transport
-$transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
-->setUsername('llegrandcontact@gmail.com')
-->setPassword('brvuxgdvdngwgmst');
+$transport = (new Swift_SmtpTransport(EMAIL_HOST, EMAIL_PORT, EMAIL_ENCRYPTION))
+->setUsername(EMAIL_USERNAME)
+->setPassword(EMAIL_PASSWORD);
 
 // Create the Mailer using your created Transport
 $mailer = new Swift_Mailer($transport);
 
 // Create a message
 $message = (new Swift_Message('Contact'))
-->setFrom(['llegrandcontact@gmail.com' => 'Laurent Legrand'])
-->setTo(['llegrandcontact@gmail.com'])
+->setFrom([EMAIL_USERNAME => 'Laurent Legrand'])
+->setTo([EMAIL_USERNAME])
 ->setReplyTo($_POST['email'], $_POST['name'])
 ->setBody($_POST['message']);
 
