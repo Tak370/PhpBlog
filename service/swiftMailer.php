e46a84729b4ff2a1ea006cf5dab35eb37c208cda
Laurent Legrand <laurentlegrandpro@icloud.com>
2020-10-21 17:13:08 +0200

    template ending

--- service/swiftMailer.php
+++ service/swiftMailer.php
@@ -1,21 +1,22 @@
 <?php
 
-require '../config/swiftMailerConfig.php';
+use App\Config;
 
 // Send an Email using SwiftMailer
 if (isset($_POST['sendmail'])) {
+    $config = Config::getInstance();
 // Create the Transport
-$transport = (new Swift_SmtpTransport(EMAIL_HOST, EMAIL_PORT, EMAIL_ENCRYPTION))
-->setUsername(EMAIL_USERNAME)
-->setPassword(EMAIL_PASSWORD);
+$transport = (new Swift_SmtpTransport($config->get('EMAIL_HOST'), $config->get('EMAIL_PORT'), $config->get('EMAIL_ENCRYPTION')))
+->setUsername($config->get('EMAIL_USERNAME'))
+->setPassword($config->get('EMAIL_PASSWORD'));
 
 // Create the Mailer using your created Transport
 $mailer = new Swift_Mailer($transport);
 
 // Create a message
 $message = (new Swift_Message('Contact'))
-->setFrom([EMAIL_USERNAME => 'Laurent Legrand'])
-->setTo([EMAIL_USERNAME])
+->setFrom([$config->get('EMAIL_USERNAME') => 'Laurent Legrand'])
+->setTo([$config->get('EMAIL_USERNAME')])
 ->setReplyTo($_POST['email'], $_POST['name'])
 ->setBody($_POST['message']);
 
