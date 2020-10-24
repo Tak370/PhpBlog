aec77f9a3114798581457b537b18b45af96a7827
Laurent Legrand <laurentlegrandpro@icloud.com>
2020-10-24 14:54:14 +0200

    add class SwiftMailer

--- service/swiftMailerConfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-const EMAIL_HOST = 'smtp.gmail.com';
-const EMAIL_PORT = 465;
-const EMAIL_USERNAME = 'llegrandcontact@gmail.com';
-const EMAIL_PASSWORD = 'brvuxgdvdngwgmst';
-const EMAIL_ENCRYPTION = 'ssl';
