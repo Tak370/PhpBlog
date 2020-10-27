7df298130e4c1235c46b80867a3b93843fe07acf
Laurent Legrand <laurentlegrandpro@icloud.com>
2020-10-27 15:15:36 +0100

    CSS colors change

--- view/post/card.php
+++ view/post/card.php
@@ -4,7 +4,7 @@
         <p class="text-muted"><?= $post->getCreatedAt()->format('d F Y')?></p>
         <p><?= $post->getExcerpt() ?></p>
         <p>
-            <a href="<?= $router->url('post', ['id' => $post->getID(), 'slug' => $post->getSlug()])?>" class="btn btn-primary">Voir plus</a>
+            <a href="<?= $router->url('post', ['id' => $post->getID(), 'slug' => $post->getSlug()])?>" class="btn btn-info">Voir plus</a>
         </p>
     </div>
 </div>
--- view/post/index.php
+++ view/post/index.php
@@ -1,10 +1,20 @@
 <?php use App\Helper\Text; ?>
-<h1>Le Blog</h1>
 
-<div class="row">
-    <?php foreach ($posts as $post): ?>
-    <div class="col-md-3">
-        <?php require 'card.php' ?>
+
+
+<section id="blog">
+    <div class="container-fluid">
+        <div class="white-divider"></div>
+        <div class="heading">
+            <h2>Le Blog</h2>
+        </div>
+        <div class="row">
+            <?php foreach ($posts as $post): ?>
+                <div class="col-md-3">
+                    <?php require 'card.php' ?>
+                </div>
+            <?php endforeach ?>
+        </div>
     </div>
-    <?php endforeach ?>
-</div>
+</section>
+
